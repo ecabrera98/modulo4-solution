441a34a26835766d6d9e57ca3e4d9461894ef9dc
Edison Cabrera <59094905+edison7905@users.noreply.github.com>
2020-09-12 09:46:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+(function () {
+var names = ["Edison", "Jonathan", "Joaquin", "Jacinto", "Pablo", "Fernanda", "Lucy", "Paula", "Laura", "Jimy"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+ 
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  
+
+   if (firstLetter=='j') {
+     byeSpeaker.speak(names[i]);
+    } else {
+     helloSpeaker.speak(names[i]);
+    }
+}
+})();
